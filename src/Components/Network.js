9529ac7b11f7e77f9124af9eb35334252a1b20e2
Jelmar Gerritsen <jelmargerritsen@gmail.com>
2020-03-24 21:52:40 +0100

    Quick setup

--- src/Components/Network.js
+++ src/Components/Network.js
@@ -1,18 +1,36 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react'
+import useInterval from '@use-it/interval'
+import { produce } from 'immer'
+
+const RADIUS = 20
+const NUM_NEURONS = 10
 
 const model = {
-  reducer: (state, { type, payload }) => {
-    switch (type) {
+  reducer: produce((draft, action) => {
+    switch (action.type) {
       case 'tick':
-        return {
-          ...state,
-          time: time + 1,
-        }
+        draft.time += 1
+        return
+      case 'update_positions':
+        action.positions.forEach(({ x, y }, i) => {
+          draft.neurons[i].position = {
+            x,
+            y,
+          }
+        })
+
+        return
+      default:
+        return
     }
-  },
+  }),
   initial_state: {
     time: 0,
-    neurons: [],
+    neurons: [...Array(NUM_NEURONS).keys()].map(() => {
+      return {
+        position: { x: null, y: null },
+      }
+    }),
     connections: [],
   },
 }
@@ -20,9 +38,61 @@ const model = {
 const Network = () => {
   const [state, dispatch] = useReducer(model.reducer, model.initial_state)
 
+  useInterval(() => dispatch({ type: 'tick' }), 1000)
+
+  useEffect(() => {
+    const { innerWidth, innerHeight } = window
+    const [width, height] = [
+      Math.floor(innerWidth / (2 * RADIUS)) - 2,
+      Math.floor(innerHeight / (2 * RADIUS)) - 2,
+    ]
+    let positions = []
+    while (positions.length < NUM_NEURONS) {
+      let position = {
+        x: Math.round(Math.random() * width * 10) / 10,
+        y: (Math.round(Math.random() * height) * 10) / 10,
+      }
+      if (
+        positions.findIndex(
+          ({ x, y }) => x === position.x && y === position.y,
+        ) === -1
+      ) {
+        positions.push({
+          x: position.x * 2 * RADIUS,
+          y: position.y * 2 * RADIUS,
+        })
+      }
+    }
+
+    dispatch({ type: 'update_positions', positions: positions })
+  }, [])
+
   return (
     <div>
-      <p>hey</p>
+      <span>{state.time}</span>
+      {state.neurons.map(neuron => {
+        const {
+          position: { x, y },
+        } = neuron
+
+        if (!x || !y) {
+          return null
+        }
+
+        return (
+          <div
+            style={{
+              position: 'absolute',
+              height: 2 * RADIUS,
+              width: 2 * RADIUS,
+              backgroundColor: 'steelblue',
+              borderRadius: '50%',
+              left: x,
+              top: y,
+            }}
+          ></div>
+        )
+      })}
     </div>
   )
 }
